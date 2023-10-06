91b6b81a8e157753400fdcebc40f6b6d38dc6cfd
Edward Jiang <ejiang@bach.local>
2023-10-05 22:25:20 -0700

    update assertion.

--- test/nodejs/duckroll_test.js
+++ test/nodejs/duckroll_test.js
@@ -7,7 +7,7 @@ describe(`duckroll extension`, () => {
     before((done) => {
         db = new duckdb.Database(':memory:', {"allow_unsigned_extensions":"true"});
         conn = new duckdb.Connection(db);
-        conn.exec(`LOAD '${process.env.QUACK_EXTENSION_BINARY_PATH}';`, function (err) {
+        conn.exec(`LOAD '${process.env.DUCKROLL_EXTENSION_BINARY_PATH}';`, function (err) {
             if (err) throw err;
             done();
         });
@@ -16,7 +16,7 @@ describe(`duckroll extension`, () => {
     it('duckroll function should return expected string', function (done) {
         db.all("SELECT duckroll('Sam') as value;", function (err, res) {
             if (err) throw err;
-            assert.deepEqual(res, [{value: "Quack Sam 🐥"}]);
+            assert.deepEqual(res, [{value: "Duckroll Sam 🐥"}]);
             done();
         });
     });
@@ -24,7 +24,7 @@ describe(`duckroll extension`, () => {
     it('duckroll_openssl_version function should return expected string', function (done) {
         db.all("SELECT duckroll_openssl_version('Michael') as value;", function (err, res) {
             if (err) throw err;
-            assert(res[0].value.startsWith('Quack Michael, my linked OpenSSL version is OpenSSL'));
+            assert(res[0].value.startsWith('Duckroll Michael, my linked OpenSSL version is OpenSSL'));
             done();
         });
     });
--- test/python/duckroll_test.py
+++ test/python/duckroll_test.py
@@ -5,9 +5,9 @@ import pytest
 # Get a fresh connection to DuckDB with the duckroll extension binary loaded
 @pytest.fixture
 def duckdb_conn():
-    extension_binary = os.getenv('QUACK_EXTENSION_BINARY_PATH')
+    extension_binary = os.getenv('DUCKROLL_EXTENSION_BINARY_PATH')
     if (extension_binary == ''):
-        raise Exception('Please make sure the `QUACK_EXTENSION_BINARY_PATH` is set to run the python tests')
+        raise Exception('Please make sure the `DUCKROLL_EXTENSION_BINARY_PATH` is set to run the python tests')
     conn = duckdb.connect('', config={'allow_unsigned_extensions': 'true'})
     conn.execute(f"load '{extension_binary}'")
     return conn
@@ -15,9 +15,9 @@ def duckdb_conn():
 def test_duckroll(duckdb_conn):
     duckdb_conn.execute("SELECT duckroll('Sam') as value;");
     res = duckdb_conn.fetchall()
-    assert(res[0][0] == "Quack Sam 🐥");
+    assert(res[0][0] == "Duckroll Sam 🐥");
 
 def test_duckroll_openssl_version_test(duckdb_conn):
     duckdb_conn.execute("SELECT duckroll_openssl_version('Michael');");
     res = duckdb_conn.fetchall()
-    assert(res[0][0][0:51] == "Quack Michael, my linked OpenSSL version is OpenSSL");
+    assert(res[0][0][0:51] == "Duckroll Michael, my linked OpenSSL version is OpenSSL");
